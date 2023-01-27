9900ad7b0477a3d2c83925b4f9b1e877a659bdc5
Federico Urbani <urbanifederico5@gmail.com>
2023-01-27 23:48:02 +0100

    updated 27

---
+++ Tester.java
@@ -0,0 +1,30 @@
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+import java.text.SimpleDateFormat;
+
+public class Tester {
+
+
+    public static void main(String[] args) {
+
+        LocalDate a=LocalDate.now();
+        System.out.println(a.lengthOfMonth()-a.getDayOfYear());
+
+        System.out.println(a.lengthOfYear()-a.getDayOfYear());
+
+
+
+
+
+
+    }
+}
+
+/**
+ * Using LocaleDate, calculate and print:
+ * how many days are left till the end of the month
+ * how many days are left till the end of the year, using the length() method from java.time.Year.of()
+ */
